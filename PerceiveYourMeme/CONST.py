e98fef759ae4d4b34e4c87ca6c804c535b90db71
Dinh Anh <dinhanhx@gmail.com>
2020-09-04 17:37:16 +0700

    Add DEFAULT_DOWNLOAD_PATH

--- PerceiveYourMeme/CONST.py
+++ PerceiveYourMeme/CONST.py
@@ -26,3 +26,6 @@ PHOTOS_HASH = {'trending':'https://knowyourmeme.com/photos/trending/page/',
 
 HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44'}
 # User-Agent headers
+
+DEFAULT_DOWNLOAD_PATH = ''
+# Define default download path
