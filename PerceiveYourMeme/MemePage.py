fc3f448725a3a9db4546073b07a9f7f0342512e1
Dinh Anh <dinhanhx@gmail.com>
2020-09-05 09:46:50 +0700

    add isValid() to check url

--- PerceiveYourMeme/MemePage.py
+++ PerceiveYourMeme/MemePage.py
@@ -1,47 +1,65 @@
 # TODO
-# 1. Refactor __init__()
+# 1. Refactor __init__() DONE
 # 2. Add checking url
 
 import urllib3
 import bs4
 from CONST import HEADERS, DEFAULT_DOWNLOAD_PATH
 
+def isValid(url):
+    if 'https://knowyourmeme.com/memes/' in url:
+        http = urllib3.PoolManager()
+        response = http.request('GET', url, headers=HEADERS)
+        if response.status == 200:
+            return True
+        else:
+            return False
+
+    else:
+        return False
+
 class MemePage():
     # An object to store basic information and template of a meme
     def __init__(self, url):
-        basic_info_dict = {}
-        # Store Meme url
-        basic_info_dict['Meme url'] = url
+        if isValid(url):
+            basic_info_dict = {}
+            # Store Meme url
+            basic_info_dict['Meme url'] = url
 
-        # Name meme
-        basic_info_dict['Name'] = url.split('/')[-1].replace('-', ' ')
+            # Name meme
+            basic_info_dict['Name'] = url.split('/')[-1].replace('-', ' ')
 
-        # Get the html document. This can be slow due to the internet
-        http = urllib3.PoolManager()
-        response = http.request('GET', url, headers=HEADERS)
-        entry_body = bs4.BeautifulSoup(response.data, 'html.parser').find('div', attrs={"class": "c", "id": "entry_body"})
+            # Get the html document. This can be slow due to the internet
+            http = urllib3.PoolManager()
+            response = http.request('GET', url, headers=HEADERS)
+            entry_body = bs4.BeautifulSoup(response.data, 'html.parser').find('div', attrs={"class": "c", "id": "entry_body"})
 
-        # Get basic information and entry tags from entry body
-        basic_info = [ele for ele in entry_body.find('dl').text.split('\n') if ele != '']
-        entry_tags = [ele for ele in
-                    entry_body.find('dl', attrs={"id":"entry_tags"}).text.split('\n') if ele != '']
+            # Get basic information and entry tags from entry body
+            basic_info = [ele for ele in entry_body.find('dl').text.split('\n') if ele != '']
+            entry_tags = [ele for ele in
+                        entry_body.find('dl', attrs={"id":"entry_tags"}).text.split('\n') if ele != '']
 
-        # Then store them
-        basic_info_dict['Unit'] = basic_info[0]
-        basic_info_dict['Status'] = basic_info[2]
-        basic_info_dict['Type'] = basic_info[4]
-        basic_info_dict['Year'] = basic_info[6]
+            # Then store them
+            basic_info_dict['Unit'] = basic_info[0]
+            basic_info_dict['Status'] = basic_info[2]
+            basic_info_dict['Type'] = basic_info[4]
+            basic_info_dict['Year'] = basic_info[6]
 
-        basic_info_dict['Tags'] = entry_tags[1]
+            basic_info_dict['Tags'] = entry_tags[1]
 
-        # Get url of template
-        self.org_img_urls = [ele['data-src'] for ele in entry_body.find('center').find_all('img')]
+            # Get url of template
+            self.org_img_urls = [ele['data-src'] for ele in entry_body.find('center').find_all('img')]
 
-        # Store url to basic_info_dict
-        basic_info_dict['Template urls'] = self.org_img_urls
+            # Store url to basic_info_dict
+            basic_info_dict['Template urls'] = self.org_img_urls
 
-        # Store basic information
-        self.basic_info_dict = basic_info_dict
+            # Store basic information
+            self.basic_info_dict = basic_info_dict
+
+        else:
+            print('Not a valid url')
+            self.basic_info_dict = {}
+            self.org_img_urls = []
 
 
     def pprint(self):
