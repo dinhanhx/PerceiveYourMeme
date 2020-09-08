6aaa83c541718d3d39093accab5b963838d957ca
Dinh Anh <dinhanhx@gmail.com>
2020-09-08 07:40:05 +0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+# The naming scheme of this package is undefined
+setup(name='PercieveYourMeme',
+        version='69.420',
+        description='The library to get memes from KnowYourMeme',
+        author='Dinh Anh Vu',
+        author_email='dinhanhx@gmail.com',
+        license='MIT',
+        packages=['PercieveYourMeme'])
