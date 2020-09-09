ddb3a52d9411bfc1b6d3848a849da19db170f725
Dinh Anh <dinhanhx@gmail.com>
2020-09-09 07:57:45 +0700

    Refactor SearchEngine(), SearchEntry(), SearchImage(), SearchNews() Add build() for SearchEngine() Add search() for SearchEntry(), SearchImage(), SearchNews()

--- PerceiveYourMeme/SearchEngine.py
+++ PerceiveYourMeme/SearchEngine.py
@@ -1,23 +1,47 @@
 import urllib3
 import bs4
 from CONST import HEADERS
+from MemePage import MemePage
+from PhotoPage import PhotoPage
+from NewsPage import NewsPage
 
 def url_maker(context, page_index, query, sort):
     return 'https://knowyourmeme.com/search?context=' + context + '&page=' + str(page_index) + '&q=' + query + '&sort=' + sort
 
 class SearchEntry():
     def __init__(self, max_pages = 1, query, sort = 'relevance'):
-        # <table class="entry_list">
+        self.max_pages = max_pages
+        self.query = query
+        self.sort = sort
+
+    def search(self):
+        # Scrap this tag <table class="entry_list">
+        # To return 2D list of MemePage objects
+        # MemePageList[search_page_index][MemePage_index_in_search_page]
         pass
 
 class SearchImage():
     def __init__(self, max_pages = 1, query, sort = 'relevance'):
-        # <div id="photo_gallery">
+        self.max_pages = max_pages
+        self.query = query
+        self.sort = sort
+
+    def search(self):
+        # Scrap this tag <div id="photo_gallery">
+        # To return 2D list of PhotoPage objects
+        # PhotoPageList[search_page_index][PhotoPage_index_in_search_page]
         pass
 
 class SearchNews():
     def __init__(self, max_pages = 1, query, sort = 'relevance'):
-        # <div id="news-posts">
+        self.max_pages = max_pages
+        self.query = query
+        self.sort = sort
+
+    def search(self):
+        # Srap this tag # <div id="news-posts">
+        # To return 2D list of NewsPages objects
+        # NewsPageList[search_page_index][NewsPage_index_in_search_page]
         pass
 
 class SearchEngine():
@@ -29,27 +53,39 @@ class SearchEngine():
         # sort : 'relevance' or 'views' or 'newest' or 'oldest'
 
         if max_pages < 1:
-            max_pages = 1
+            self.max_pages = 1
+        else:
+            self.max_pages = max_pages
 
         if context is not in ['entries', 'images', 'news']:
-            context = 'entries'
+            self.context = 'entries'
+        else:
+            self.context = context
 
         if sort is not in ['relevance', 'views', 'newest', 'oldest']:
-            sort = 'relevance'
+            self.sort = 'relevance'
+        else:
+            self.sort = sort
 
-        if type(query) == type(' '):
+        if type(query) == type('') && query != '':
             # Format query to have the valid format
-            query = query.replace(' ', '+', -1)
-
-            if context == 'entries':
-                return SearchEntry(max_pages=max_pages, query=query, sort=sort)
-
-            if context == 'images':
-                return SearchImage(max_pages=max_pages, query=query, sort=sort)
-
-            if context == 'news':
-                return SearchNews(max_pages=max_pages, query=query, sort=sort)
-
-
+            self.query = query.replace(' ', '+', -1)
         else:
             print('Query is not a string')
+            self.query = ''
+
+
+    def build(self):
+        # Build object SearchEntry, SearchImage, SearchNews
+        if self.query = '':
+            print('No object is build')
+            return None
+        else:
+            if self.context == 'entries':
+                return SearchEntry(context=self.context, max_pages=self.max_pages, query=self.query, sort=self.sort)
+
+            if self.context == 'images':
+                return SearchImage(context=self.context, max_pages=self.max_pages, query=self.query, sort=self.sort)
+
+            if self.context == 'news':
+                return SearchNews(context=self.context, max_pages=self.max_pages, query=self.query, sort=self.sort)
