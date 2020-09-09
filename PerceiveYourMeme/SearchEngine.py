9abab31f905ec34187f5a7f5db09f3c9efe0e486
Dinh Anh <dinhanhx@gmail.com>
2020-09-09 21:16:45 +0700

    Fix SearchEngine and Implement search() Resolve syntatic erros

--- PerceiveYourMeme/SearchEngine.py
+++ PerceiveYourMeme/SearchEngine.py
@@ -1,6 +1,6 @@
 import urllib3
 import bs4
-from CONST import HEADERS
+from CONST import HEADERS, KYM
 from MemePage import MemePage
 from PhotoPage import PhotoPage
 from NewsPage import NewsPage
@@ -9,7 +9,7 @@ def url_maker(context, page_index, query, sort):
     return 'https://knowyourmeme.com/search?context=' + context + '&page=' + str(page_index) + '&q=' + query + '&sort=' + sort
 
 class SearchEntry():
-    def __init__(self, max_pages = 1, query, sort = 'relevance'):
+    def __init__(self, query, max_pages = 1, sort = 'relevance'):
         self.max_pages = max_pages
         self.query = query
         self.sort = sort
@@ -18,10 +18,23 @@ class SearchEntry():
         # Scrap this tag <table class="entry_list">
         # To return 2D list of MemePage objects
         # MemePageList[search_page_index][MemePage_index_in_search_page]
-        pass
+
+        http = urllib3.PoolManager()
+
+        MemePageList = []
+
+        for page_index in range(1, self.max_pages+1):
+            url = url_maker('entries', page_index, self.query, self.sort)
+            response = http.request('GET', url, headers=HEADERS)
+            tag_a_list = bs4.BeautifulSoup(response.data, 'html.parser').find('table', attrs={'class':'entry_list'}).find_all('a', attrs={'class':'photo'})
+            url_list = [KYM+tag_a['href'] for tag_a in tag_a_list]
+            MemePageList.append([MemePage(u_r_l) for u_r_l in url_list])
+
+        self.MemePageList = MemePageList
+
 
 class SearchImage():
-    def __init__(self, max_pages = 1, query, sort = 'relevance'):
+    def __init__(self, query, max_pages = 1, sort = 'relevance'):
         self.max_pages = max_pages
         self.query = query
         self.sort = sort
@@ -30,10 +43,25 @@ class SearchImage():
         # Scrap this tag <div id="photo_gallery">
         # To return 2D list of PhotoPage objects
         # PhotoPageList[search_page_index][PhotoPage_index_in_search_page]
-        pass
+
+        # If use this to get multiple images, name of PhotoPage onject will be blank
+
+        http = urllib3.PoolManager()
+
+        PhotoPageList = []
+
+        for page_index in range(1, self.max_pages+1):
+            url = url_maker('images', page_index, self.query, self.sort)
+            response = http.request('GET', url, headers=HEADERS)
+            tag_a_list = bs4.BeautifulSoup(response.data, 'html.parser').find('div', attrs={'id':'photo_gallery'}).find_all('a', attrs={'class':'photo'})
+            url_list = [KYM+tag_a['href'] for tag_a in tag_a_list]
+            PhotoPageList.append([PhotoPage(u_r_l) for u_r_l in url_list])
+
+        self.PhotoPageList = PhotoPageList
+
 
 class SearchNews():
-    def __init__(self, max_pages = 1, query, sort = 'relevance'):
+    def __init__(self, query, max_pages = 1, sort = 'relevance'):
         self.max_pages = max_pages
         self.query = query
         self.sort = sort
@@ -42,11 +70,26 @@ class SearchNews():
         # Srap this tag # <div id="news-posts">
         # To return 2D list of NewsPages objects
         # NewsPageList[search_page_index][NewsPage_index_in_search_page]
-        pass
+
+        http = urllib3.PoolManager()
+
+        NewsPageList = []
+
+        for page_index in range(1, self.max_pages+1):
+            url = url_maker('news', page_index, self.query, self.sort)
+            response = http.request('GET', url, headers=HEADERS)
+            soup = bs4.BeautifulSoup(response.data, 'html.parser')
+            url_list = ['https:'+h1.find('a')['href'] for h1 in soup.find('div', attrs={"id":"entries"}).find_all('div')[1].find_all('h1')]
+            url_list = [u_r_l.replace(':443', '') for u_r_l in url_list]
+            print(url_list)
+            NewsPageList.append([NewsPage(u_r_l) for u_r_l in url_list])
+
+        self.NewsPageList = NewsPageList
+
 
 class SearchEngine():
     # A class to search for MemePage, PhotoPage, VideoPage
-    def __init__(self, context = 'entries', max_pages = 1, query, sort = 'relevance'):
+    def __init__(self, query, context = 'entries', max_pages = 1, sort = 'relevance'):
         # context : 'entries' or 'images' or 'news'
         # max_pages : a positive number
         # query : a string, for example 'Elon Musk'
@@ -57,17 +100,17 @@ class SearchEngine():
         else:
             self.max_pages = max_pages
 
-        if context is not in ['entries', 'images', 'news']:
+        if context not in ['entries', 'images', 'news']:
             self.context = 'entries'
         else:
             self.context = context
 
-        if sort is not in ['relevance', 'views', 'newest', 'oldest']:
+        if sort not in ['relevance', 'views', 'newest', 'oldest']:
             self.sort = 'relevance'
         else:
             self.sort = sort
 
-        if type(query) == type('') && query != '':
+        if type(query) == type('') and query != '':
             # Format query to have the valid format
             self.query = query.replace(' ', '+', -1)
         else:
@@ -77,15 +120,15 @@ class SearchEngine():
 
     def build(self):
         # Build object SearchEntry, SearchImage, SearchNews
-        if self.query = '':
+        if self.query == '':
             print('No object is build')
             return None
         else:
             if self.context == 'entries':
-                return SearchEntry(context=self.context, max_pages=self.max_pages, query=self.query, sort=self.sort)
+                return SearchEntry(query=self.query, max_pages=self.max_pages, sort=self.sort)
 
             if self.context == 'images':
-                return SearchImage(context=self.context, max_pages=self.max_pages, query=self.query, sort=self.sort)
+                return SearchImage(query=self.query, max_pages=self.max_pages, sort=self.sort)
 
             if self.context == 'news':
-                return SearchNews(context=self.context, max_pages=self.max_pages, query=self.query, sort=self.sort)
+                return SearchNews(query=self.query, max_pages=self.max_pages, sort=self.sort)
