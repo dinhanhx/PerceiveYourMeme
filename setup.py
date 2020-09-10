7cdf5d7977219a52167bdbe02a99ff849df0f983
Dinh Anh <dinhanhx@gmail.com>
2020-09-10 07:27:36 +0700

    Fix typos

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 # The naming scheme of this package is undefined
-setup(name='PercieveYourMeme',
+setup(name='PerceiveYourMeme',
         version='69.420',
         description='The library to get memes from KnowYourMeme',
         author='Dinh Anh Vu',
         author_email='dinhanhx@gmail.com',
         license='MIT',
-        packages=['PercieveYourMeme'])
+        packages=['PerceiveYourMeme'])
