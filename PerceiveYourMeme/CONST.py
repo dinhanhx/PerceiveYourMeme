cffc5034ff8d476fa0d6b22fad144df161a345d9
Dinh Anh <dinhanhx@gmail.com>
2020-09-12 17:12:26 +0700

    Update comments

--- PerceiveYourMeme/CONST.py
+++ PerceiveYourMeme/CONST.py
@@ -10,7 +10,7 @@ KYM_HASH = {'memes':'https://knowyourmeme.com/memes/page/',
 
 MEMES_HASH = {'popular':'https://knowyourmeme.com/memes/popular/page/',
                 'submissions':'https://knowyourmeme.com/memes/submissions/page/'}
-# MEMES_HASH : a set of directories
+# MEMES_HASH : a set of sub-directories
 # append a positive integer to get the corresponding page number
 # For example, https://knowyourmeme.com/memes/popular/page/1 will be the page.
 
@@ -21,9 +21,12 @@ MEMES_SORT_HASH = {'views':'?sort=views', 'comments':'?sort=comments'}
 
 PHOTOS_HASH = {'trending':'https://knowyourmeme.com/photos/trending/page/',
                 'most-commented':'https://knowyourmeme.com/photos/most-commented/page/'}
-# MEMES_HASH : a set of directories
+# PHOTOS_HASH : a set of sub-directories
 # append a positive integer to get the corresponding page number
 # For example, https://knowyourmeme.com/memes/trending/page/1 will be the page.
+
+# Why don't PHOTOS_HASH have PHOTOS_SORT_HASH likes MEMES_HASH?
+# Because KYM developers treat sort just like sub-directories
 
 HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44'}
 # User-Agent headers
