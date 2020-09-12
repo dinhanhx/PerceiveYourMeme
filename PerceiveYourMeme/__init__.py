3799464cbedc80ea0fb8c448efbe533506666a1f
Dinh Anh <dinhanhx@gmail.com>
2020-09-13 06:20:23 +0700

    Add GetKYM

--- PerceiveYourMeme/__init__.py
+++ PerceiveYourMeme/__init__.py
@@ -3,3 +3,4 @@ from .PhotoPage import PhotoPage
 from .NewsPage import NewsPage
 from .CONST import *
 from .SearchEngine import SearchEngine, SearchEntry, SearchImage, SearchNews
+from .GetKYM import get_memes, get_photos, get_news
