0caf3b7255d2b894b55487a213eae20106a734e2
Dinh Anh <dinhanhx@gmail.com>
2020-09-12 19:30:12 +0700

    Implemented scrapping codes

--- PerceiveYourMeme/GetKYM.py
+++ PerceiveYourMeme/GetKYM.py
@@ -5,10 +5,18 @@
 import urllib3
 import bs4
 from .CONST import *
+from .NewsPage import NewsPage
+
+def get_soup(url):
+    http = urllib3.PoolManager()
+    response = http.request('GET', url, headers=HEADERS)
+    soup = bs4.BeautifulSoup(response.data, 'html.parser')
+    return soup
 
 def get_memes(directory = None, page_index = 1, sort = ''):
     # directory : None or 'popular' or 'submissions'
     # page_index : a positive integer
+    # To return a list of MemePage objects
     url = ''
 
     if page_index < 1:
@@ -23,11 +31,17 @@ def get_memes(directory = None, page_index = 1, sort = ''):
             url = KYM_HASH['memes'] + str(page_index) + MEMES_SORT_HASH[sort]
 
 
-    pass
+    soup = get_soup(url)
+
+    tag_a_list = soup.find('table', attrs={'class':'entry_list'}).find_all('a', attrs={'class':'photo'})
+    url_list = [KYM+tag_a['href'] for tag_a in tag_a_list]
+
+    return [MemePage(u_r_l) for u_r_l in url_list]
 
 def get_photos(directory = None, page_index = 1):
     # directory : None or 'trending' or 'most-commented'
     # page_index : a positive integer
+    # To return a list of PhotoPage objects
     url = ''
 
     if page_index < 1:
@@ -42,12 +56,24 @@ def get_photos(directory = None, page_index = 1):
             url = KYM_HASH['photos'] + str(page_index)
 
 
-    pass
+    soup = get_soup(url)
+
+    tag_a_list = soup.find('div', attrs={'id':'photo_gallery'}).find_all('a', attrs={'class':'photo'})
+    url_list = [KYM+tag_a['href'] for tag_a in tag_a_list]
+
+    return [PhotoPage(u_r_l) for u_r_l in url_list]
 
 def get_news(page_index = 1):
     # page_index : a positive integer
+    # To return a list of NewsPage objects
     if page_index < 1:
         page_index = 1
 
     url = KYM_HASH['news'] + str(page_index)
-    pass
+
+    soup = get_soup(url)
+
+    url_list = ['https:'+h1.find('a')['href'] for h1 in soup.find('div', attrs = {'id': 'maru'}).find_all('div')[1].find_all('h1')]
+    url_list = [u_r_l.replace(':443', '') for u_r_l in url_list]
+
+    return [NewsPage(u_r_l) for u_r_l in url_list]
