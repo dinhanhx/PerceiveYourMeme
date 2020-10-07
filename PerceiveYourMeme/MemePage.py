071e59f875e97d2b5fd559f7494b8ee5535da69e
Dominika Krejci <445553@mail.muni.cz>
2020-10-07 15:33:33 +0200

    Fix PEP8 in MemePage

--- PerceiveYourMeme/MemePage.py
+++ PerceiveYourMeme/MemePage.py
@@ -8,17 +8,16 @@ import urllib3
 import bs4
 from .CONST import HEADERS, DEFAULT_DOWNLOAD_PATH
 
+
 def isValid(url):
     if 'https://knowyourmeme.com/memes/' in url:
         http = urllib3.PoolManager()
         response = http.request('GET', url, headers=HEADERS)
-        if response.status == 200:
-            return True
-        else:
-            return False
+        return response.status == 200
 
     else:
         return False
+
 
 class MemePage():
     # An object to store basic information and template of a meme
@@ -36,12 +35,18 @@ class MemePage():
             response = http.request('GET', url, headers=HEADERS)
             soup = bs4.BeautifulSoup(response.data, 'html.parser')
             try:
-                entry_body = soup.find('div', attrs={"class": "c", "id": "entry_body"})
+                entry_body = soup.find('div',
+                                       attrs={"class": "c",
+                                              "id": "entry_body"})
 
                 # Get basic information and entry tags from entry body
-                basic_info = [ele for ele in entry_body.find('dl').text.split('\n') if ele != '']
-                entry_tags = [ele for ele in
-                            entry_body.find('dl', attrs={"id":"entry_tags"}).text.split('\n') if ele != '']
+                dl = entry_body.find('dl').text.split('\n')
+                basic_info = [ele for ele in dl if ele != '']
+
+                dl_entry = entry_body.find('dl',
+                                           attrs={"id": "entry_tags"})
+                dl_entry = dl_entry.text.split('\n')
+                entry_tags = [ele for ele in dl_entry if ele != '']
 
                 # Then store them
                 self.basic_info_dict['Unit'] = basic_info[0]
@@ -61,7 +66,8 @@ class MemePage():
                 # Get url of template
                 self.org_img_urls = []
                 if entry_body.find('center') is not None:
-                    self.org_img_urls = [ele['data-src'] for ele in entry_body.find('center').find_all('img')]
+                    imgs = entry_body.find('center').find_all('img')
+                    self.org_img_urls = [ele['data-src'] for ele in imgs]
 
                 # Store url to basic_info_dict
                 self.basic_info_dict['Template urls'] = self.org_img_urls
@@ -80,14 +86,12 @@ class MemePage():
             self.basic_info_dict = {}
             self.org_img_urls = []
 
-
     def pprint(self):
         # Pretty print of basic_info_dict
         from json import dumps
         print(dumps(self.basic_info_dict, indent=3))
 
-
-    def download_origin_image(self, custom_path = DEFAULT_DOWNLOAD_PATH):
+    def download_origin_image(self, custom_path=DEFAULT_DOWNLOAD_PATH):
         # Download images
         # then name them corresponding to self.basic_info_dict['Name']
         # Use attributes self.org_img_urls
@@ -97,14 +101,16 @@ class MemePage():
             for org_img_url in self.org_img_urls:
                 response = http.request('GET', org_img_url, HEADERS)
                 file_type = response.headers['Content-Type'].split('/')[-1]
-                fname_path = DEFAULT_DOWNLOAD_PATH + self.basic_info_dict['Name'] + ' ' + str(i)
+                fname_path = (DEFAULT_DOWNLOAD_PATH +
+                              self.basic_info_dict['Name'] +
+                              ' ' +
+                              str(i))
                 with open(fname_path+'.'+file_type, 'wb') as f:
                     f.write(response.data)
 
                 i += 1
 
             return True
-
 
         else:
             print('Org img urls are blank')
