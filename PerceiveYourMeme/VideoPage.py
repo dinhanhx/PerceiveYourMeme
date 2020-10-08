a0650419254bab84653a06d5f9bbda4694116328
Rolv Apneseth <rolv.apneseth@gmail.com>
2020-10-08 14:35:55 +0100

    Shortened isValid function and started trying to get direct youtube url for video

--- PerceiveYourMeme/VideoPage.py
+++ PerceiveYourMeme/VideoPage.py
@@ -2,20 +2,17 @@ import urllib3
 import bs4
 from json import dumps
 
-from .CONST import HEADERS, DEFAULT_DOWNLOAD_PATH
+from CONST import HEADERS, DEFAULT_DOWNLOAD_PATH
+
 
 
 def isValid(url):
     if url.startswith('https://knowyourmeme.com/videos/'):
         http = urllib3.PoolManager()
         response = http.request('GET', url, headers=HEADERS)
-        if response.status == 200:
-            return True
-        else:
-            return False
 
-    else:
-        return False
+        return response.status == 200
+    return False
 
 
 class VideoPage():
@@ -26,16 +23,35 @@ class VideoPage():
         self.basic_info_dict = {}
 
         if isValid(url):
-            self.basic_info_dict['Video url'] = url
+            self.basic_info_dict['Original url'] = url
 
             # Get name and id of video from url
             id_name = url.split('/')[-1].split('-')
             self.basic_info_dict['Id'] = id_name[0]
-            self.basic_info_dict['Name'] = ' '.join(id_name[1:])  
+            self.basic_info_dict['Name'] = ' '.join(id_name[1:])
+
+            # Get soup
+            http = urllib3.PoolManager()
+            response = http.request('GET', url, headers=HEADERS)
+            soup = bs4.BeautifulSoup(response.data, 'html.parser')
+            # Get direct video url
+            # try:
+            dir_url = soup.find("iframe", attrs={"class": "embedly-embed"})
+            print(dir_url)  
+            # Returns where the document is embedded but I have no idea how to then search inside this document to get the direct url
+
+            #     self.basic_info_dict['Direct url'] = dir_url
+            # except:
+            #     print('No direct video url could be found')
         else:
             print('Not a valid url')
 
     def pprint(self):
         """Pretty print of self.basic_info_dict"""
         print(dumps(self.basic_info_dict, indent=3))
-            
+   
+
+if __name__ == '__main__':
+    western_animation = VideoPage('https://knowyourmeme.com/videos/225020-western-animation')
+    western_animation.pprint()
+
