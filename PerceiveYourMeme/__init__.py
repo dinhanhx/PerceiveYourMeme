5dce430ddb68d3a251e282211a8eb971b2b43c07
Dinh Anh <dinhanhx@gmail.com>
2020-10-30 09:33:15 +0700

    add VideoPage

--- PerceiveYourMeme/__init__.py
+++ PerceiveYourMeme/__init__.py
@@ -1,6 +1,7 @@
 from .MemePage import MemePage
 from .PhotoPage import PhotoPage
 from .NewsPage import NewsPage
+from .VideoPage import VideoPage
 from .CONST import *
 from .SearchEngine import SearchEngine, SearchEntry, SearchImage, SearchNews
 from .GetKYM import get_memes, get_photos, get_news
